33cb39f2204fde8d9eedc0092ab0afaafe6a6b27
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-05-12 23:11:18 +0300

    create main  CRUD logic

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,23 +1,75 @@
+const { HttpError } = require("../helpers");
 const {
   listContactsService,
   getContactByIdService,
+  addContactService,
+  updateContactService,
+  removeContactService,
 } = require("../services/contactsServices");
 
 const listContacts = async (req, res, next) => {
-  const contacts = await listContactsService();
-  res.json(contacts);
+  try {
+    const contacts = await listContactsService();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getContactById = async (req, res, next) => {
-  const contact = await getContactByIdService(req.params.contactId);
+  const { contactId } = req.params;
+  try {
+    const contact = await getContactByIdService(contactId);
 
-  res.json(contact);
+    if (!contact) {
+      throw HttpError(404, `Contact with ${contactId} not found`);
+    }
+
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports = { listContacts, getContactById };
+const addContact = async (req, res, next) => {
+  try {
+    const contact = await addContactService(req.body);
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-// listContacts,
-// getContactById,
-// removeContact,
-// addContact,
-// updateContact,
+const updateContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    const contact = await updateContactService(contactId, req.body);
+    if (!contact) {
+      throw HttpError(404, `Contact with ${contactId} not found`);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const removeContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    const removedContact = await removeContactService(contactId);
+    if (!removedContact) {
+      throw HttpError(404, `Contact with ${contactId} not found`);
+    }
+    res.status(200).json(removedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact,
+};
--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -2,6 +2,9 @@ const express = require("express");
 const {
   listContacts,
   getContactById,
+  addContact,
+  updateContact,
+  removeContact,
 } = require("../../controllers/contactsControllers");
 
 const router = express.Router();
@@ -10,16 +13,10 @@ router.get("/", listContacts);
 
 router.get("/:contactId", getContactById);
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.post("/", addContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.delete("/:contactId", removeContact);
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.put("/:contactId", updateContact);
 
 module.exports = router;
