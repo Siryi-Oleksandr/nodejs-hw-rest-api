bec9d9638079d5e85b45a1bc89e0d353f25902d9
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-05-24 12:36:32 +0300

    add logic filter contacts by favotire

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -6,12 +6,19 @@ const controllerWrapper = require("../helpers/controllerWrapper");
 
 const getContacts = controllerWrapper(async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner }, "-createdAt -updatedAt", {
+  const filter = { owner };
+  if (favorite && favorite === "true") {
+    filter.favorite = true;
+  }
+  if (favorite && favorite === "false") {
+    filter.favorite = false;
+  }
+  const contacts = await Contact.find(filter, "-createdAt -updatedAt", {
     skip,
     limit,
-  }).populate("owner", "name");
+  }).populate("owner", "email subscription");
   res.json(contacts);
 });
 
